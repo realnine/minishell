3d2b9da87558938396c80b4d929aa664ee5a760f
realnine <ljk0252g@gmail.com>
2021-11-23 21:51:30 +0900

    11/23 (pm 9:51)

--- srcs/redirect.c
+++ srcs/redirect.c
@@ -1,25 +1,28 @@
 # include "../minishell.h"
 
+// 멀티라인 입력 모드
 int	input_multiline(t_cmd *cmd)
 {
-	char *buf;
+	char	*buf;
+	int		flags;
+	int		fd;
 
-	// 멀티라인 입력 모드
-	cmd->fd_in = open("tmp_redi_multiline", O_RDWR | O_CREAT, \
-			S_IRWXU | S_IRWXG | S_IRWXO);
+	flags = O_WRONLY | O_CREAT | O_TRUNC;
+	fd = open("tmp_redi_multiline", flags, 0777);
 	if (cmd->fd_in < 0)
-		return (error_print(cmd->redi_in_arg, strerror(errno), NULL, 1));
+		return (err_print(strerror(errno), 1));
 	cmd->input_file = ft_strdup("tmp_redi_multiline");
 	while (1)
 	{
 		buf = readline("> ");
 		if (ft_strcmp(buf, cmd->redi_in_arg) == 0)
 			break ;
-		ft_putstr_fd(buf, cmd->fd_in);
-		ft_putstr_fd("\n", cmd->fd_in);
+		ft_putstr_fd(buf, fd);
+		ft_putstr_fd("\n", fd);
 		free(buf);
 	}
 	free(buf);
+	close(fd);
 	return (RET_TRUE);
 }
 
@@ -29,11 +32,11 @@ int	redirect_in(t_cmd *cmd)
 	{
 		if (input_multiline(cmd) == RET_FALSE)
 			return (RET_FALSE);
+		cmd->fd_in = open(cmd->input_file, O_RDONLY | O_CREAT, 0777);
 	}
 	else
 	{
-		cmd->fd_in = open(cmd->redi_in_arg, O_RDWR, \
-				S_IRWXU | S_IRWXG | S_IRWXO);
+		cmd->fd_in = open(cmd->redi_in_arg, O_RDWR, 0777);
 		if (cmd->fd_in < 0)
 			return (error_print(cmd->redi_in_arg, strerror(errno), NULL, 1));
 	}
@@ -42,17 +45,19 @@ int	redirect_in(t_cmd *cmd)
 
 int	redirect_out(t_cmd *cmd)
 {
+	int		flags;
+
 	if (ft_strcmp(cmd->redi_out, ">>") == 0)
 	{
-		cmd->fd_out = open(cmd->redi_out_arg, O_RDWR | O_CREAT | O_APPEND, \
-			S_IRWXU | S_IRWXG | S_IRWXO);
+		flags = O_RDWR | O_CREAT | O_APPEND;
+		cmd->fd_out = open(cmd->redi_out_arg, flags, 0777);
 		if (cmd->fd_out < 0)
 			return (error_print(cmd->redi_out_arg, strerror(errno), NULL, 1));
 	}
 	else
 	{
-		cmd->fd_out = open(cmd->redi_out_arg, O_RDWR | O_CREAT | O_TRUNC, \
-			S_IRWXU | S_IRWXG | S_IRWXO);
+		flags = O_RDWR | O_CREAT | O_TRUNC;
+		cmd->fd_out = open(cmd->redi_out_arg, flags, 0777);
 		if (cmd->fd_out < 0)
 			return (error_print(cmd->redi_out_arg, strerror(errno), NULL, 1));
 	}
